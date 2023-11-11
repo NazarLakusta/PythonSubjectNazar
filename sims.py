cc63ce8506e56022e218c9d5a378603b3b7ee897
Назар Лакуста <n.lakusta@step.local>
2023-11-11 13:45:36 +0200

    third commit sims

--- sims.py
+++ sims.py
@@ -1,5 +1,6 @@
+from random import *
 class Human:
-    def __init__(self,name,job,home,car):
+    def __init__(self,name,job=None,home=None,car=None):
         self.name = name
         self.job = job
         self.home = home
@@ -8,18 +9,29 @@ class Human:
         self.satiety = 50
         self.money = 100
 
+    def get_home(self):
+        self.home = House
 
+    def get_car(self):
+        self.car = Auto()
 
 
 
 class Auto:
 
-    def __init__(self):
-        self.brand = 0
-        self.strength = 0
-        self.consumption = 0
-        self.fuel = 0
+    def __init__(self, brand_list):
+        self.brand = choice(list(brand_list))
+        self.strength = brand_list[self.brand]["strength"]
+        self.consumption = brand_list[self.brand]["consumption"]
+        self.fuel = brand_list[self.brand]["fuel"]
 
+
+brands_of_car = {
+ "BMW":{"fuel":100, "strength":100, "consumption":6},
+ "Lada":{"fuel":50, "strength":40, "consumption":10},
+ "Ferrari":{"fuel":80, "strength":120, "consumption":14},
+
+}
 class House:
     def __init__(self):
         self.mess = 0
@@ -31,3 +43,5 @@ class Job:
         self.job = 0
         self.salary = 0
         self.gladness = 0
+
+
